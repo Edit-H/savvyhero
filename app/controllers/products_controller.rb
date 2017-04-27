a61eee3ff25883db0c7943490f0df35b54789438
Edit Hartoonians <arnith12@gmail.com>
2017-04-27 13:18:01 -0700

    product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+  def index
+    session[:item] ||=0
+    @item = session[:item]
+  def show
+    @products=Product.all
+  end
+  end
+
+  def product
+    render "products/index"
+  end
+
+
+
+end
